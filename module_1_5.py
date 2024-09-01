264e7299e7f5d5652fd6427ea2f11cfd4797860e
EvgenyRedko <binito1992@mail.ru>
2024-09-01 17:52:28 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1,2,3, 'food'
+print(immutable_var)
+#immutable_var[0] = 150 Кортеж относится к неизменяемым типам данных
+mutable_list = [1,2,3, 'food']
+print(mutable_list)
+mutable_list.remove(2)
+print(mutable_list)
